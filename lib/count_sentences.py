a3290b5533d0925e678f965d138bf2b3aec31804
JedwardMook <jedwardmook@yahoo.com>
2023-06-28 12:39:58 -0500

    Tests passing

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value=''):
+      self._value = value
+
+  def get_value(self):
+     return self._value
+  
+  def set_value(self, value):
+     if (type(value) == str):
+        self._value = value
+     else:
+        print("The value must be a string.")
+
+  value = property(get_value, set_value)
+      
+  def is_sentence(self):
+    return self._value.endswith(".")
+  
+  def is_question(self):
+     return self._value.endswith("?")
+  
+  def is_exclamation(self):
+    return self._value.endswith("!")
+  
+  def count_sentences(self):
+     if self._value == "":
+        return 0   
+     no_exclaim = self._value.replace("!", ".")
+     no_question = no_exclaim.replace("?", ".")
+     split = no_question.split(". ")
+     return (len(split))
+     
+  
+
